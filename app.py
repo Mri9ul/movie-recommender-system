866366439e042fd02aaf35d0a14fb79ea8a59d94
Mridul Karar <109417471+Mri9ul@users.noreply.github.com>
2026-04-18 12:58:53 +0200

    Replace API key in fetch_poster function
    Updated API key in fetch_poster function for security.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 def fetch_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=3e97d48bd5c86852586e4cb0c8626a5d&language=en-US".format(movie_id))
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key="XXXX"&language=en-US".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
 

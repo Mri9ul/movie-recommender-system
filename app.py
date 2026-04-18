25960a2c83589dcb8cbde66c205dc46e2b20e7ec
Mridul Karar <109417471+Mri9ul@users.noreply.github.com>
2026-04-18 12:59:47 +0200

    Change selectbox prompt for movie selection

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ similarity = pickle.load(open("similarity.pkl","rb"))
 
 st.title("Movie Recommender System")
 
-selected_movie_name = st.selectbox("how would you like to be contacted?",
+selected_movie_name = st.selectbox("Select a movie:",
                       movies["title"].values)
 
 if st.button("Recommend"):
